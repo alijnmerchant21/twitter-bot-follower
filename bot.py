c4990bd76d29de4e3f8fcf71484f12833f4e7fdc
alijnmerchant21 <amercha1@stevens.edu>
2023-05-07 13:29:58 +0100

    Major updates
    Solve error - API.get_user() takes 1 positional argument but 2 were given
    
    Add each followed user to csv file
    
    Add keyword in bio to follow people

--- bot.py
+++ bot.py
@@ -23,9 +23,12 @@ api = tweepy.API(auth)
 # Set the minimum number of followers required to follow an account
 MIN_FOLLOWERS = 100
 
+# Set the keywords to search for in the user's bio
+KEYWORDS = ["blockchain", "web3"]
+
 # Set the number of accounts to follow in a day
 MAX_FOLLOW_PER_DAY = 400
-FOLLOW_PER_HOUR = 25
+FOLLOW_PER_HOUR = 15
 
 # Set the number of hours after which to unfollow an account
 UNFOLLOW_AFTER_HOURS = 36
@@ -61,7 +64,7 @@ follow_count = 0
 for follower_id in tweepy.Cursor(api.get_follower_ids).items():
     try:
         # Check if the follower has a minimum number of followers
-        user = api.get_user(follower_id)
+        user = api.get_user(user_id=follower_id)
         if user.followers_count < MIN_FOLLOWERS:
             continue
 
@@ -69,8 +72,12 @@ for follower_id in tweepy.Cursor(api.get_follower_ids).items():
         if any(account["id"] == str(user.id) for account in followed_accounts):
             continue
 
+        # Check if the user's bio contains any of the keywords
+        if not any(keyword in user.description.lower() for keyword in KEYWORDS):
+            continue
+
         # Follow the user
-        api.create_friendship(user.id)
+        api.create_friendship(user_id=user.id)
         followed_accounts.append({
             "id": str(user.id),
             "display_name": user.name,
@@ -79,23 +86,33 @@ for follower_id in tweepy.Cursor(api.get_follower_ids).items():
         follow_count += 1
         print(f"Followed user {user.id}")
 
+        # Save the newly followed account to the CSV file
+        with open(CSV_FILE_NAME, "a", newline="") as csvfile:
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerow([str(user.id), user.name, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")])
+
         # Check if the daily follow limit has been reached
         if follow_count >= MAX_FOLLOW_PER_DAY:
             break
 
+         # Check if the hourly follow limit has been reached
+        if follow_count % FOLLOW_PER_HOUR == 0:
+            print("Hourly follow limit reached, bot is going to sleep.")
+            time.sleep(3600)  # Sleep for 1 hour
+
     except tweepy.errors.NotFound:
         print(f"User with ID {follower_id} not found.")
 
-
-
+    # Check if the daily follow limit has been reached
+    if follow_count >= MAX_FOLLOW_PER_DAY:
+        break
 
 # Save the updated list of followed accounts to the CSV file
 with open(CSV_FILE_NAME, "w", newline="") as csvfile:
     csvwriter = csv.writer(csvfile)
     csvwriter.writerow(["id", "display_name", "followed_at"])
     for account in followed_accounts:
-        csvwriter.writerow([f'"{account["id"]}"', account["display_name"], account["followed_at"].strftime("%Y-%m-%d %H:%M:%S")])
-
+        csvwriter.writerow([account["id"], account["display_name"], account["followed_at"].strftime("%Y-%m-%d %H:%M:%S")])
 
 # Check if the daily follow limit was reached
 if follow_count >= MAX_FOLLOW_PER_DAY:
@@ -124,4 +141,4 @@ with open(CSV_FILE_NAME, "w", newline="") as csvfile:
     csvwriter.writerow(["id", "display_name", "followed_at"])
     for account in followed_accounts:
         csvwriter.writerow([account["id"], account["display_name"], account["followed_at"].strftime("%Y-%m-%d %H:%M:%S")])
-
+        
